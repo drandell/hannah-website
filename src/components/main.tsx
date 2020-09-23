fdcae3abc034da679bc5688cf577de40de60bae5
Daniel Randell <danielrandell@outlook.com>
2020-09-23 23:06:30 +0100

    fix routing on github pages

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Home } from './home';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ContactMe } from './contact-me';
 
 export class Main extends Component {
 
     render() {
         return (
-            <Router>
-                <Route path="/contact-me" exact component={ContactMe} /> 
-                <Route path="/" exact component={Home} />
+            <Router basename={`${process.env.PUBLIC_URL}/`}>
+                <Switch>
+                    <Route exact path='/contact-me' component={ContactMe} /> 
+                    <Route exact path='/' component={Home} />
+                </Switch>
             </Router>
         );
     }
