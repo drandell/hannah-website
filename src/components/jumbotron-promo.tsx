16ab4468e64f47f610e3c53484b3b48d1e274539
Daniel Randell <drandell@users.noreply.github.com>
2021-05-23 13:40:06 +0100

    remove promo alt text

--- src/components/jumbotron-promo.tsx
+++ src/components/jumbotron-promo.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Component } from 'react';
-//import newpromo from '../images/promo.jpg'; // Tell webpack this JS file uses this image
 import newpromo from '../images/promo-2.jpg'; // Tell webpack this JS file uses this image
 
 export class JumbotronPromo extends Component {
@@ -14,7 +13,7 @@ export class JumbotronPromo extends Component {
       return (
           <>
             <section>
-              <img src={newpromo} alt="Promo" style={this.style} />
+              <img src={newpromo} alt="" style={this.style} />
             </section>
           </>
       );
